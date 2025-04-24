26976655b8cfc7cb49f05a0c3dcf40933ce52fa0
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2025-04-24 06:41:04 +0000

    24-04-2025 06:41:01

--- MyApp/app/(tabs)/index.jsx
+++ MyApp/app/(tabs)/index.jsx
@@ -1,10 +1,18 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
 import React from 'react'
+
+import icedCoffeeImg from "@/assets/images/iced-coffee.png"
 
 const app = () => {
   return (
     <View style={styles.container}>
+      <ImageBackground
+        source={icedCoffeeImg}
+        resizeMode='cover'
+        style={styles.image}
+      >
       <Text style={styles.text}>Coffee Shop</Text>
+      </ImageBackground>
     </View>
   )
 }
@@ -16,11 +24,20 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     },
+    image:{
+        width:'100%',
+        height:'100%',
+        flex:1,
+        resizeMode:'cover',
+        justifyContent:'center',
+
+    },
     text: {
       color: 'white',
       fontSize: 42,
       fontWeight: 'bold',
       textAlign: 'center',
+      backgroundColor:'rgba(0,0,0,0.5)'
 
     }
 })
